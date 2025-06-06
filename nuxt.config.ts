912ffea6f48078d69f550b29184e17fc9a71d0a6
Ahtasham-Shaikh <ahtasham.shaikh@houzeo.com>
2025-06-06 15:41:05 +0530

    First Commit

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+  css: ['~/assets/css/base.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+})
---
+++ pages/shows/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1 class="">This is the shows page</h1>
+</template>
+
+<script setup>
+
+</script>
